b64fe61c0a3e4bfab19abc5b819bf277e3911d7e
Mushnik <cmorrison@Coreys-Mac-mini.local>
2019-06-15 17:51:38 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -10,19 +10,24 @@ console.log("User flipped " + cardThree);
 console.log("User flipped " + cardFour); */
 
 var cards = ["queen", "queen", "king", "king"];
-
 var cardsInPlay = []; 
+let cardId
 
-var cardOne = cards[0];
-cardsInPlay.push("cardOne");
-console.log("User flipped" + cardOne); 
-
-var cardTwo = cards[2];
-cardsInPlay.push("cardThree");
-console.log("User flipped" + cardThree);
-
-if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-	alert("You've found a match!")
+function checkForMatch () {
+if (cardsInPlay[0] === cardsInPlay[1]) {
+	console.log("you found a match!");
 	} else {
-		alert ("Sorry, try again.");
+	console.log("Sorry, try again.");
+	}
+};
+
+function flipCard (cardId) {
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	if (cardsInPlay.length === 2) {
+		checkForMatch();
+	}
+};
+
+flipCard(0);
+flipCard(2);
